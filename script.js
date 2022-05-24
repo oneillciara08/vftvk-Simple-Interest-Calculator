a7906532e3bd06c4eecc4567f4d5828cb0df3507
oneillciara08 <oneillciara08@gmail.com>
2022-05-24 22:06:58 +0000

    changes made from theia lab environment

--- script.js
+++ script.js
@@ -1,6 +1,30 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
+   var principal= document.getElementById("principal").value;
+   var rate= document.getElementById("rate").value;
+   var years= document.getElementById("years").value;
+   var result= principal * years * rate/100;
+   var years_modified= new Date().getFullYear()+parseInt(years);
+
+   if (principal == "" || principal <= 0)
+    {
+        alert("Please enter a positive number");
+        document.getElementById("principal").focus();
+        return false;
+    }
+
+   document.getElementById("result").innerHTML = "If you deposit <mark>"
+   + principal + "</mark>, <br/> at an interest rate of <mark>"
+   + rate +"%.</mark><br/> You will receive an amount of <mark>" + result +
+   "</mark>,<br/>in the year <mark>" + years_modified + ".</mark><br/>";
     
 }
+
+function updateRate()
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
+}
+
         
+    
